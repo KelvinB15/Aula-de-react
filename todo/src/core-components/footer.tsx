7ac02c09ae1bd702f02297c915164f7697ae5724
kelvin-trabalho <kelvinkelvinbarroso@gmail.com>
2025-11-17 11:11:38 -0300

    Fundamentos avanãdos, com requisições na API

--- todo/src/core-components/footer.tsx
+++ todo/src/core-components/footer.tsx
@@ -7,9 +7,14 @@ export default function Footer() {
             <nav className="flex items-center justify-center gap-4">
                 <NavLink to="/">
                     <Text variant="body-sm-bold" className="text-gray-300">
-                        Roda pé
+                        Home
                     </Text>
                 </NavLink>
+                 <NavLink to="/componentes">
+          <Text variant="body-sm-bold" className="text-gray-300">
+            Componentes
+          </Text>
+        </NavLink>
             </nav>
         </footer>
     )
